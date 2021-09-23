561283fe1d90a85f65841ba7c4fbde7ab3b7056e
Rasmus Saame <rasmussaame@gmail.com>
2021-09-23 13:17:05 +0300

    muudatused

--- script.js
+++ script.js
@@ -14,7 +14,31 @@ function fizzbuzz(n) {
 
 function fibonacci() {
     let fib = [0, 1]
-    
+    while (fib.length < 100) {
+        fib.push(fib[fib.length - 1] + fib[fib.length - 2])
+    }
+    console.log(fib.join("\n"))
 }
 
-fizzbuzz(50)
+function prime_generator(first) {
+    let primes = []
+    let possible = []
+    for (let i = 2; i <= first; i++) {
+        possible.push(i)
+    }
+    while (possible.length > 0) {
+        let prime = possible.splice(0, 1)[0]
+        primes.push(prime)
+        let k = 2
+        while (k * prime < first) {
+            let i = possible.indexOf(k * prime)
+            if (i != -1) {
+                possible.splice(i, 1);
+            }
+            k++;
+        }
+    }
+    console.log(primes.join("\n"))
+}
+
+prime_generator(47)
