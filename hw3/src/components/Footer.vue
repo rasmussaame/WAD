c8c9bb0ca70e1d8f5d0837097c8712979bd61642
Kaarel Vesilind <kaarel.vesilind@askrobin.com>
2021-11-21 16:17:41 +0200

    Fixed some stylings

--- hw3/src/components/Footer.vue
+++ hw3/src/components/Footer.vue
@@ -35,9 +35,8 @@ h2 {
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: absolute;
   width: 100%;
   height: 200px;
-  background: lightgoldenrodyellow
+  background: lightgoldenrodyellow;
 }
 </style>
