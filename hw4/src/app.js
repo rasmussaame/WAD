4cf16c6d8a8317308cde46b9b5f51937356bb768
Kaarel Vesilind <kaarel.vesilind@askrobin.com>
2021-11-30 11:43:12 +0200

    Removed vue files and changed .ejs file names

--- hw4/src/app.js
+++ hw4/src/app.js
@@ -7,16 +7,12 @@ app.set('view engine', 'ejs');
 
 // listen for requests on port 3000
 app.listen(3000);
-/* app.get() is used to respond to Get requests, and it takes two arguments:
-1- arg1: represents what path/url you want to listen to (e.g., '/' listens to index path)
-2- arg2: represents a function that takes in request and response objects */
+// TODO teeme mingi eraldi index lehe ka???
 app.get('/', (req, res) => {
-  /* res.sendFile('./views/index.html', { root: __dirname }); */
   res.render('index');
 });
 
 app.get('/posts', async (req, res) => {
-  /* res.sendFile('./views/index.html', { root: __dirname }); */
   try {
     console.log("get posts request has arrived");
     const posts = await pool.query(
@@ -29,12 +25,14 @@ app.get('/posts', async (req, res) => {
   
 });
 
-app.get('/contactus', (req, res) => {
-  /* res.sendFile('./views/index.html', { root: __dirname }); */
-  res.render('contactus');
+app.get('/singlepost', (req, res) => {
+  res.render('singlepost');
+});
+
+app.get('/addnewpost', (req, res) => {
+  res.render('addnewpost');
 });
 
 app.use((req, res) => {
-  /* res.status(404).sendFile('./views/404.html', { root: __dirname });*/
   res.status(404).render('404');
 });
