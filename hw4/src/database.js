2a967829ac2408130952b13493c6da8837f40833
Kaarel Vesilind <kaarel.vesilind@askrobin.com>
2021-11-30 11:12:41 +0200

    hw4 base

---
+++ hw4/src/database.js
@@ -0,0 +1,9 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+ user: "postgres",
+ password: "postgres",
+ database: "WAD",
+ host: "localhost",
+ port: "5432"
+});
+module.exports = pool;
